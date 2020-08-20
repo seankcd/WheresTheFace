55d5ebe26f6fb3d28e7289cdda3ec7d65a938a6e
Sean Courtney <seankc@me.com>
2020-08-19 17:18:44 -0700

    App.js fixes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/*
-      <FaceRecognition />
-      */}
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm />
+        {/*
+        <FaceRecognition />
+        */}
+      </div>
+    );
+  }
 }
 
 export default App;
