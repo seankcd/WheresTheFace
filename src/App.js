7599370a13d99becb6ebfffdc24a0f37444c6359
Sean Courtney <seankc@me.com>
2020-08-19 17:16:09 -0700

    fixes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 import './App.css';
 
 function App() {
@@ -8,8 +10,9 @@ function App() {
     <div className="App">
       <Navigation />
       <Logo />
-      {/*
+      <Rank />
       <ImageLinkForm />
+      {/*
       <FaceRecognition />
       */}
     </div>
