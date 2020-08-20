222546e1cce9ee1896ff847fff165d14e5d159b4
Sean Courtney <seankc@me.com>
2020-08-19 18:08:29 -0700

    Added clarifai api

--- src/App.js
+++ src/App.js
@@ -1,21 +1,53 @@
 import React, { Component } from 'react';
+import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
 
+const app = new Clarifai.App({
+  apiKey: '9a3c6dc728104e7fa8e76edf7eb8a717'
+});
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+      app.models.predict(
+        Clarifai.COLOR_MODEL, 
+        "https://samples.clarifai.com/face-det.jpg"
+      ).then(
+      function(response) {
+        console.log(response)
+      },
+      function(err) {
+        // there was an error
+      }
+    );
+  }
+
   render() {
     return (
       <div className="App">
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
-        {/*
+        <ImageLinkForm 
+          onInputChange={this.onInputChange} 
+          onButtonSubmit={this.onButtonSubmit}
+        />
         <FaceRecognition />
-        */}
       </div>
     );
   }
---
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FaceRecognition = () => {
+    return (
+        <div className='center pa3'>
+            <img alt='Face' src={'https://samples.clarifai.com/face-det.jpg'} />
+        </div>
+    );
+}
+
+export default FaceRecognition;
