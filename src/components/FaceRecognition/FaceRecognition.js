1660d13e268066d556897fda3f088cd3359b8420
Sean Courtney <seankc@me.com>
2020-08-20 17:09:41 -0700

    Added face recognition box

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import './FaceRecognition.css';
 
-const FaceRecognition = ({ imageUrl }) => {
+const FaceRecognition = ({ imageUrl, box }) => {
     return (
-        <div className='center'>
-            <div className='pa3'>
-                <img alt='' src={imageUrl} width='700px' height='auto' />
+        <div className='center ma'>
+            <div className='absolute pa3'>
+                <img id='inputimage' alt='' src={imageUrl} width='500px' height='auto' />
+                <div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
             </div>
         </div>
     );
 }
 
 export default FaceRecognition;
+
