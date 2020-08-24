3f44ad4bd35ea0b560525df254f059f078ce3716
Sean Courtney <seankc@me.com>
2020-08-23 17:34:48 -0700

    Spacing on forms fixed

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -3,7 +3,7 @@ import './Signin.css';
 
 const Signin = ({ onRouteChange }) => {
     return (
-        <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+        <article className="br3 ba b--black-10 w-100 w-50-m w-25-l mw6 shadow-5 center">
             <main className="pa4 black-80">
                 <div className="measure">
                     <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
