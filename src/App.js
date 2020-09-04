3925fedfd5c46b1782b174d13056bac275566625
Sean Courtney <seankc@me.com>
2020-09-03 18:39:51 -0700

    host names

--- src/App.js
+++ src/App.js
@@ -62,7 +62,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-      fetch('http://localhost:3000/imageurl', {
+      fetch('https://nameless-coast-34092.herokuapp.com/imageurl', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
@@ -72,7 +72,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://nameless-coast-34092.herokuapp.com:3000/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
